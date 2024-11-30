94f8bc0d0da0d2ba16faabc867ce7eaea30037be
Rakesh Munukuntla <130857014+Rakeshmunukuntla@users.noreply.github.com>
2024-11-30 10:22:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-function insert_Row() {
+
+let cell=1;
+
+function insert_Row() 
+{
+	const table = document.getElementById("sampleTable");
+	
     //Write your code here
-  
-  
+
+  const newRow=table.insertRow(0);
+		
+	const col1=newRow.insertCell(0);
+	const col2=newRow.insertCell(1);
+
+	col1.textContent="New Cell"+cell;
+	cell++;
+	col2.textContent="New Cell"+cell;
+	cell++;
+ 
 }
